418e300d75cf6d07f677ea4bd89c6e62d651e480
Andreas S <k0haku@hotmail.com>
2019-06-13 21:35:16 +0200

    stuff

---
+++ Source/TicTacToe/Public/TicTacToe_ImpossibleAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "TicTacToe_ImpossibleAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TICTACTOE_API ATicTacToe_ImpossibleAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
