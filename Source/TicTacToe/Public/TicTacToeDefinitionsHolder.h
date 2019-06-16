13f8bc9e6028c7f7cc81cba3ead2b69b3a2f950f
Andreas S <k0haku@hotmail.com>
2019-06-16 13:59:53 +0200

    add BlockType enum

---
+++ Source/TicTacToe/Public/TicTacToeDefinitionsHolder.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/ObjectMacros.h"
+
+
+UENUM()
+enum class EBlockType : uint8
+{
+	None,
+	Cross,
+	Circle,
+};
+
+/**
+ * 
+ */
+// class TICTACTOE_API TicTacToeDefinitionsHolder
+// {
+// public:
+// 	TicTacToeDefinitionsHolder();
+// 	~TicTacToeDefinitionsHolder();
+
+// };
